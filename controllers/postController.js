c428befd938d48609e8b777b15330367da7b7e82
LuigiRac <lui.rac06@gmail.com>
2025-01-26 16:45:22 +0100

    bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -21,13 +21,29 @@ function destroy(req, res) {
 function show(req, res) {
     const { id } = req.params;
     const query = 'SELECT * FROM posts WHERE id = ?';
+
+
+    const tagsSql = `SELECT T.* 
+    FROM tags AS T
+    JOIN post_tag AS PT ON T.id = PT.tag_id
+    WHERE PT.post_id = ? `;
+
     connection.query(query, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
-        res.json(results[0]);
+        // res.json(results[0]);
+
+        const posts = results[0];
+
+        connection.query(tagsSql, [id], (err, tagResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+            posts.tags = tagResults;
+            res.json(posts);
+
+        })
     })
-
-
 }
+
 
 module.exports = { index, destroy, show };
